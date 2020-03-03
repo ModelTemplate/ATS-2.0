8fcad5d64788e7477b7055faefb8a6d9f30ad250
Thomas <37784432+ModelTemplate@users.noreply.github.com>
2020-03-02 21:01:03 -0800

    Selena's changes

--- BalanceInquiry.java
+++ BalanceInquiry.java
@@ -1,1 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atmcasestudy;
 
+/**
+ *
+ * @author dx200
+ */
+public class BalanceInquiry extends Transaction{
+    
+;
+    private int account;
+    private Screen screen;
+    private BankDatabase database;
+
+    
+    public BalanceInquiry(int accountNumber, Screen atmScreen, BankDatabase atmDatabase){
+        super(accountNumber, atmScreen,atmDatabase);
+        account = accountNumber;
+        screen = atmScreen;
+        database = atmDatabase;
+}
+
+  
+    @Override
+    public void execute(){
+        screen.displayMessage("Your account balance is: ");
+        screen.displayDollarAmount(database.getTotalBalance(account));
+        
+    }
+    
+}
