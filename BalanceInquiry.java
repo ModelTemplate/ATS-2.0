95abdf276374c1e8deb982b047d466e4d963bb80
Thomas <37784432+ModelTemplate@users.noreply.github.com>
2020-03-05 21:09:33 -0800

    Update BalanceInquiry.java

--- BalanceInquiry.java
+++ BalanceInquiry.java
@@ -1,35 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package atmcasestudy;
 
 /**
- *
- * @author dx200
+ * BalanceInquiry class
+ * @author ATS-2.0
  */
-public class BalanceInquiry extends Transaction{
-    
-;
+public class BalanceInquiry extends Transaction {
     private int account;
     private Screen screen;
     private BankDatabase database;
 
-    
-    public BalanceInquiry(int accountNumber, Screen atmScreen, BankDatabase atmDatabase){
+    public BalanceInquiry(int accountNumber, Screen atmScreen, BankDatabase atmDatabase) {
         super(accountNumber, atmScreen,atmDatabase);
-        account = accountNumber;
-        screen = atmScreen;
-        database = atmDatabase;
-}
-
-  
-    @Override
-    public void execute(){
-        screen.displayMessage("Your account balance is: ");
-        screen.displayDollarAmount(database.getTotalBalance(account));
-        
+        this.account = super.getAccountNumber();
+        this.screen = super.getScreen();
+        this.database = super.getBankDatabase();
     }
-    
+
+    // overridden, inherited abstract method from superclass
+    // executes balance inquiry use case
+    public void execute() {
+        screen.displayMessageLine("Your account balance: ");
+        screen.displayMessage("Available balance: ");
+        screen.displayDollarAmount(bankDatabase.getAvailableBalance(this.account));
+        screen.displayMessage("\nTotal balance: ");
+        screen.displayDollarAmount(bankDatabase.getTotalBalance(this.account) + "\n");
+    }
 }
