0695391f830a4e3e97db9222a84fd6c828e44a88
Thomas <37784432+ModelTemplate@users.noreply.github.com>
2020-03-05 20:05:13 -0800

    Updated with Melvin's code + fixes

--- Withdrawal.java
+++ Withdrawal.java
@@ -5,27 +5,45 @@ package atmcasestudy;
  * @author ATS-2.0
  */
 public class Withdrawal extends Transaction {
+    // fields inherited from superclass:
+    // int accountNumber
+    // Screen screen
+    // BankDatabase bankDataBase
     private Keypad keypad;
     private CashDispenser dispenser;
-    private int withdrawAmount;
-    private int account;
-    private Screen screen;
-    private BankDatabase database;
+    private boolean isCashDispensed = false;
+    private final static int CANCELED = 6;  // keypad input to cancel withdrawal
 
-    public Withdrawal(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase, Keypad keypad, CashDispenser cashDispenser) {
-    	super(userAccountNumber, atmScreen,atmBankDatabase);
-        this.keypad = keypad;
-        dispenser = cashDispenser;
-        account = userAccountNumber;
-        screen = atmScreen;
-        database = atmBankDatabase;
+    public Withdrawal(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase, Keypad atmKeypad, CashDispenser atmCashDispenser) {
+    	super(userAccountNumber, atmScreen, atmBankDatabase);
+        this.keypad = atmKeypad;
+        dispenser = atmCashDispenser;
     }
 
+    // overridden, inherited abstract method from superclass
+    // executes withdrawal use case
     public void execute() {
-        screen.displayMessage("Enter the amount to withdraw: ");
-        withdrawAmount = keypad.getInput();
-        dispenser.dispenseCash(withdrawAmount);
-        database.debit(account, withdrawAmount);
-        screen.displayMessage("Please take your cash.");
+        screen.displayMessage("\nEnter the amount to withdraw: ");
+        int withdrawAmount = keypad.getInput();
+
+        if (withdrawAmount != CANCELED) {
+            int availableBalance = bankDataBase.getAvailableBalance(getAccountNumber());
+            if (widthdrawAmount <= availableBalance) {
+                if (dispenser.isSufficientCashAvailable(withdrawAmount)) {
+                    bankDataBase.debit(getAccountNumber(), withdrawAmount);
+                    cashDispenser.dispenseCash(withdrawAmount);
+                    isCashDispensed = true;
+                    screen.displayMessageLine( "\nYour cash has been dispensed. Please take your cash now.");
+                } else {
+                    screen.displayMessageLine( "\nInsufficient funds available in the ATM.\n\nPlease choose a smaller amount.");
+                }
+            }
+        } else {
+            screen.displayMessageLine("\nCanceling transaction...");
+        }
+
+        if (!isCashDispensed && withdrawAmount != CANCELED) {
+            execute();
+        }
     }
 }
