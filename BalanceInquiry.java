58d8370122a7dac4cd5e7cd8736ae5ba9af2562e
Thomas <37784432+ModelTemplate@users.noreply.github.com>
2020-03-05 21:18:00 -0800

    Update BalanceInquiry.java

--- BalanceInquiry.java
+++ BalanceInquiry.java
@@ -1,1 +1,29 @@
+package atmcasestudy;
 
+/**
+ * BalanceInquiry class
+ * @author ATS-2.0
+ */
+public class BalanceInquiry extends Transaction {
+    private int account;
+    private Screen screen;
+    private BankDatabase bankDatabase;
+
+    public BalanceInquiry(int accountNumber, Screen atmScreen, BankDatabase atmDatabase) {
+        super(accountNumber, atmScreen,atmDatabase);
+        this.account = super.getAccountNumber();
+        this.screen = super.getScreen();
+        this.bankDatabase = super.getBankDatabase();
+    }
+
+    // overridden, inherited abstract method from superclass
+    // executes balance inquiry use case
+    public void execute() {
+        screen.displayMessageLine("\nYour account balance:");
+        screen.displayMessage("Available balance: ");
+        screen.displayDollarAmount(bankDatabase.getAvailableBalance(this.account));
+        screen.displayMessage("\nTotal balance: ");
+        screen.displayDollarAmount(bankDatabase.getTotalBalance(this.account));
+        screen.displayMessageLine("");
+    }
+}
